844f6d44aa13553c2b67f8ccea92b8d933dff02d
Yogun1 <sebastian__scherer@hotmail.com>
2022-10-29 15:24:47 -0300

    Funcion para carga de disco
    Funcion para carga de disco

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
 'use strict';
 
 /*
- * APELLIDO, NOMBRE | APELLIDO, NOMBRE
+ * SEBASTIAN, SCHERER
  */
 
 // Ejemplo de la estructura de un disco:
-// let disco = {
-//     Nombre: 'El lado oscuro de la Programación',
-//     Autor: 'Los Programadores Anónimos',
-//     Codigo: 1,
-//     Pistas: [
-//         {
-//             Nombre: 'Esa cajita loca llamada variablecita',
-//             Duracion: 200,
-//         },
-//         {
-//             Nombre: 'Nunca quise ser un NaN',
-//             Duracion: 180,
-//         },
-//         {
-//             Nombre: 'No quiero programar',
-//             Duracion: 90,
-//         },
-//         {
-//             Nombre: 'Bajo presión',
-//             Duracion: 240,
-//         },
-//         {
-//             Nombre: 'La odisea de las variables privadas',
-//             Duracion: 120,
-//         },
-//         {
-//             Nombre: 'Sr. Programador',
-//             Duracion: 720,
-//         },
-//     ],
-// };
-
+let disco = {
+    Nombre: '',
+    Autor: '',
+    codigo: 0,
+    Pistas: [
+        {
+            Nombre: '',
+            Duracion: 0,  
+        }
+    ]
+}
 // Discos:
 let discos = [];
 
 // Función Cargar:
-const Cargar = () => {
-    // Cositas:
+const cargar = () => {
+    let codigo;
+
+    do {
+        codigo = +prompt('Ingrese un codigo')
+        //validacion
+    } while (codigo <= 0 || codigo >= 1000 || isNaN(codigo));
+    // pide las pistas 
+
+    //Generar un lugar donde guardar las pistas
+    /*let pistas = [];
+
+    // pedir las pistas
+    do {
+        //pedir el nombre
+        //Pedir la duracion
+        //guardar la pista
+
+        let pista = {
+            nombre: 'NOMBRE',
+            duracion: 'DURACION'
+        };
+
+        pistas.push(pista)
+
+    }
+    while (confirm('Ingrese la pista')) {
+
+    }*/
+
+    // genera el disco 
+    const nuevoDisco = {
+        ...disco,
+        codigo: codigo
+    };
+    //Guardamos el disco
+    discos.push(nuevoDisco);
 };
 
 // Función Mostrar:
-const Mostrar = () => {
+
+ const mostrar = () => {
     // Variable para ir armando la cadena:
+    console.log(this.disco)
+    const cantidadDeDiscos = discos.length;
     let html = '';
 
     // Cositas:
@@ -57,3 +74,31 @@ const Mostrar = () => {
 };
 
 // Todas las funciones que necesites:
+
+// para cambiar el color
+
+let duracion = 80;
+
+const MostrarColor = function () {
+
+        let color = 'red'
+
+        if (duracion <= 80) {
+            color = red;
+        } else {
+            //cambio el color a green
+        }
+    
+    html =  `<p>aca conecto con front</p>`;
+
+    document.getElementById('info').innerHTML = html;
+
+}
+// para la funcion mostrar  pued hacer un 
+//console.log(disco.pista)
+
+
+// puedo usar array asosciativo para los discos y pistas
+
+//let array = ['Alejandro', 'Diaz', 'true']
+//let array2 = []
